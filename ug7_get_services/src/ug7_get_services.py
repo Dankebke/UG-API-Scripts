79c072ce7f3d486be5196466d4088cf31cdc5e60
Lopatin Danila <132710987+Dankebke@users.noreply.github.com>
2025-01-24 12:46:50 +0300

    Add files via upload

---
+++ ug7_get_services/src/ug7_get_services.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname("__file__"), '.')))
+import xmlrpc.client
+import xlwt
+from xlwt import Workbook
+
+SERVER = '192.168.84.26'
+USER = 'API_Admin' 	
+PASSWORD = 'Sup3rS#cretP@ssword!
+
+server = xmlrpc.client.ServerProxy('http://' + SERVER + ':4040/rpc', verbose=False)
+
+res = server.v2.core.login(USER, PASSWORD, {})
+auth_token = res['auth_token']
+services = server.v1.libraries.services.list(auth_token, 0, 1000, {}, [])
+
+wb = Workbook()
+style_1 = xlwt.XFStyle()
+style_1.alignment.wrap = 1
+sheet1 = wb.add_sheet('Services')
+style_2 = xlwt.easyxf('font: bold 1')
+sheet1.write(0,0,'Services',style_2)
+sheet1.write(0,1,'Ports',style_2)
+y = 1
+service_names = []
+for item in services['items']:
+    x = 0
+    protocol_arr = []
+    sheet1.write(y,x,item['name'],style_1)
+    x += 1
+    for protocol in item['protocols']:
+        protocol_type = protocol['proto'] + '/'
+        protocol_port = protocol['port'] + '\n'
+        protocol_arr.append(protocol_type + protocol_port)
+    protocol_arr[-1] = protocol_arr[-1].replace('\n', '')
+    sheet1.write(y,x,protocol_arr,style_1)
+    y += 1
+
+file_path = os.path.join('Абсолютный', 'путь', 'до', 'создаваемого', 'файла.xls')
+wb.save(file_path)
+
+server.v2.core.logout(auth_token)
