b108ca5aeb529fe15a79e8f93796f1fcf9b75598
Lopatin Danila <132710987+Dankebke@users.noreply.github.com>
2025-01-28 10:59:06 +0300

    Update ug7_get_services.py

--- ug7_get_services/src/ug7_get_services.py
+++ ug7_get_services/src/ug7_get_services.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 
 import os
-import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname("__file__"), '.')))
 import xmlrpc.client
 import xlwt
 from xlwt import Workbook
